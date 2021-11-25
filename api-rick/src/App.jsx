a873a4400ec4d6d6c4c7b134442aff974cbf3ac2
leoor22 <leonelrivas.rl@gmail.com>
2021-11-24 23:13:02 -0300

    added new project of API Rick

---
+++ api-rick/src/App.jsx
@@ -0,0 +1,12 @@
+import Formulario from "./Formulario";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>App Rick And Morty</h1>
+      <Formulario />
+    </div>
+  );
+};
+
+export default App;
---
+++ api-rick/src/Formulario.jsx
@@ -0,0 +1,10 @@
+const Formulario = () => {
+  return (
+    <div>
+      <h2>Formulario</h2>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Formulario;
