41c2d4d2de6c549362a6f69fc8cffa460ae704cc
leoor22 <leonelrivas.rl@gmail.com>
2021-11-30 23:37:00 -0300

    new project: blogg app

---
+++ router-v6/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import App from "./App";
+import Blog from "./routes/Blog";
+import Contacto from "./routes/Contacto";
+import Inicio from "./routes/Inicio";
+import NoEncontrada from "./routes/NoEncontrada";
+import Post from "./routes/Post";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Inicio />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="blog/:id" element={<Post />} />
+        <Route path="contacto" element={<Contacto />} />
+        <Route path="*" element={<NoEncontrada />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+
+  document.getElementById("root")
+);
